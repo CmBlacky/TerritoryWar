74d8c912e8f14ad38faf6ce02e1acf976e9c176e
Blacky <charliebarbaud@gmail.com>
2023-05-11 18:05:29 +0200

    Update

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import pygame
-import random
 
 # Dimensions de la fenêtre
 WINDOW_WIDTH = 800
@@ -11,7 +10,20 @@ CELL_SIZE = WINDOW_WIDTH // MAP_SIZE
 
 # Couleurs
 GRAY = (150, 150, 150)
+DARK_GRAY = (130, 130, 130)
 
+class Camp:
+    def __init__(self, color, symbol, initial_credits):
+        self.color = color
+        self.symbol = symbol
+        self.credits = initial_credits
+        self.territory = set()
+
+def initialize_camps():
+    camps = []
+    camps.append(Camp("red", "R", 50))
+    camps.append(Camp("blue", "B", 50))
+    return camps
 
 def draw_map(camps, screen):
     screen.fill(GRAY)  # Remplir l'écran avec la couleur grise
@@ -20,12 +32,32 @@ def draw_map(camps, screen):
         for y in range(MAP_SIZE):
             cell_rect = pygame.Rect(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE)
             pygame.draw.rect(screen, GRAY, cell_rect)
-            pygame.draw.rect(screen, (100, 100, 100), cell_rect, 1)  # Contour plus foncé
+            pygame.draw.rect(screen, DARK_GRAY, cell_rect, 1)  # Contour plus foncé
 
     for camp in camps:
-        for cell in camp.get_territory():
+        for cell in camp.territory:
             x, y = cell
             cell_rect = pygame.Rect(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE)
             pygame.draw.rect(screen, camp.color, cell_rect)
 
     pygame.display.flip()  # Rafraîchir l'écran
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+    pygame.display.set_caption("Simulation de jeu de territoire")
+
+    camps = initialize_camps()
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        draw_map(camps, screen)
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
